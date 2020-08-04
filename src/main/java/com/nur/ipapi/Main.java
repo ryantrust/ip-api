acac8c55aec6dd1421319243ce77a22234728897
jM1pOpJhre-mJaMOFOMt <53131588+jM1pOpJhre-mJaMOFOMt@users.noreply.github.com>
2020-08-03 22:18:21 -0700

    renamed Main class

--- src/main/java/com/nur/ipapi/Main.java
+++ src/main/java/com/nur/ipapi/Main.java
@@ -5,8 +5,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main
 {
     public static final String MODID = "IPAPI";
     public static final String VERSION = "1.0";
