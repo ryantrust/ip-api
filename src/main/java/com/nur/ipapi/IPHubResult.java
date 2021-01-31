56289bdc3dfdba218dd58d4c1e19a3b2f6219036
jM1pOpJhre-mJaMOFOMt <53131588+jM1pOpJhre-mJaMOFOMt@users.noreply.github.com>
2021-01-31 14:03:41 -0800

    Update IPHubResult.java

--- src/main/java/com/nur/ipapi/IPHubResult.java
+++ src/main/java/com/nur/ipapi/IPHubResult.java
@@ -1,5 +1,29 @@
 package com.nur.ipapi;
 
+import net.minecraft.util.EnumChatFormatting;
+
 public enum IPHubResult {
-    GOOD, MIXED, BAD
+    GOOD(0,"Good", EnumChatFormatting.GREEN+"Good"), BAD(1,"Bad",EnumChatFormatting.RED+"Bad"), MIXED(2,"Mixed", EnumChatFormatting.GOLD+"Mixed");
+
+    public int getBlockCode() {
+        return blockCode;
+    }
+
+    public String getUnformattedString() {
+        return unformattedString;
+    }
+
+    public String getFormattedString() {
+        return formattedString;
+    }
+
+    private final int blockCode;
+    private final String unformattedString;
+    private final String formattedString;
+
+    IPHubResult(int blockCode, String unformattedString, String formattedString) {
+        this.blockCode = blockCode;
+        this.unformattedString = unformattedString;
+        this.formattedString = formattedString;
+    }
 }
