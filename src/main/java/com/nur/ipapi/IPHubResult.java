272de12625ebede994a222be678fdaae50ec367f
jM1pOpJhre-mJaMOFOMt <53131588+jM1pOpJhre-mJaMOFOMt@users.noreply.github.com>
2021-02-01 12:35:08 -0800

    Improved IPHubResult enum

--- src/main/java/com/nur/ipapi/IPHubResult.java
+++ src/main/java/com/nur/ipapi/IPHubResult.java
@@ -3,7 +3,14 @@ package com.nur.ipapi;
 import net.minecraft.util.EnumChatFormatting;
 
 public enum IPHubResult {
-    GOOD(0,"Good", EnumChatFormatting.GREEN+"Good"), BAD(1,"Bad",EnumChatFormatting.RED+"Bad"), MIXED(2,"Mixed", EnumChatFormatting.GOLD+"Mixed");
+    GOOD(0,"Good", EnumChatFormatting.GREEN+"Good", EnumChatFormatting.GREEN+"(Good)"), BAD(1,"Bad",EnumChatFormatting.RED+"Bad", EnumChatFormatting.RED+"(Bad)"), MIXED(2,"Mixed", EnumChatFormatting.GOLD+"Mixed", EnumChatFormatting.GOLD+"(Mixed)");
+
+    public static IPHubResult fromBlock(int blockInt) {
+        for(IPHubResult result : values()){
+            if(result.getBlockCode() == blockInt) return result;
+        }
+        return null;
+    }
 
     public int getBlockCode() {
         return blockCode;
@@ -17,13 +24,19 @@ public enum IPHubResult {
         return formattedString;
     }
 
+    public String getFormattedStringWithParentheses() {
+        return formattedStringWithParentheses;
+    }
+
     private final int blockCode;
     private final String unformattedString;
     private final String formattedString;
+    private final String formattedStringWithParentheses;
 
-    IPHubResult(int blockCode, String unformattedString, String formattedString) {
+    IPHubResult(int blockCode, String unformattedString, String formattedString, String formattedStringWithParentheses) {
         this.blockCode = blockCode;
         this.unformattedString = unformattedString;
         this.formattedString = formattedString;
+        this.formattedStringWithParentheses = formattedStringWithParentheses;
     }
 }
